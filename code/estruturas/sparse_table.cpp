490f509333e24c833a5a8723969abeb0941ac23c
lucianovr <rluciano025@gmail.com>
2019-08-18 12:39:58 -0300

    Atualizando código do sparse table

--- code/estruturas/sparse_table.cpp
+++ code/estruturas/sparse_table.cpp
@@ -1,14 +1,27 @@
-// Consulta intervalo em O(1), mas não suporta update
-const int MAX = 1e3+3;
-int N, st[22][MAX];
-int a[MAX];
+/*
+Resolve problemas de consulta a intervalos (RSQ, RMQ etc) de um vetor
+estatico, ou seja, os valores nao sofrem update.
+Alterar a funcao comb() de acordo (min, max, soma etc)
+Pre-processamento O(NlogN) e consulta em O(1).
+N = tamanho do vetor a[]
+a[] deve ser indexado em 0
+*/
+const int MAXN = (1e6 + 1);
+#define LOGN	(21)
+int st[MAXN][LOGN];
+int N, a[MAXN];
 
-void SparseTable() {
-	rep(j, 0, 20) for(int i = 0; (i+(1<<j)-1) < N; i++)
-		st[0][i][j] = j ? min(st[i][j-1], st[i+(1<<(j-1))][j-1]) : a[i];
+int comb(int left, int right)
+{
+	return min(left, right);
 }
 
-int query(int l, int r) {		// return minimo no intervalo (l, r)
-	int k = log2(r-l+1);
-	return min(st[l][k] < st[r-(1<<k)+1][k]);
+void SparseTable() {
+	rep(k, 0, LOGN) for(int i = 0; (i + (1<<k) - 1) < N; i++)
+		st[i][k] = k ? comb(st[i][k-1], st[i + (1<<(k-1))][k - 1]) : a[i];
+}
+
+int query(int l, int r) {
+	int k = log2(r - l + 1);
+	return comb(st[l][k] , st[r - (1<<k) + 1][k]);
 }
