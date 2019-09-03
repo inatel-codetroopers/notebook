0529be32692a5f56d088842ae80b42ed59af0c55
lucianovr <rluciano025@gmail.com>
2019-09-03 19:40:27 -0300

    Fast pow fixed.

--- code/matematica/fast-pow/fast_pow.cpp
+++ code/matematica/fast-pow/fast_pow.cpp
@@ -1,8 +1,20 @@
-// Calcula (B^P)%MOD em O(logP). Calcula o inverso modular de b(modulo
-// mod) se mod for primo. Basta fazer invB = fastpow(b,mod-2,mod)
-ll fastpow(ll b, ll p, ll mod) {
-  ll ret = 1;
-  for (ll pot = b; p > 0; p >>= 1, pot = (pot * pot) % mod)
-    if (p & 1) ret = (ret * pot) % mod;
+/**
+ * fastpow() realiza exponenciacao rapida de inteiros
+ * #param ll b - base da exponenciacao
+ * #param ll expo - expoente
+ * #param ll mod - o resultado sera calculado modulo este valor
+ * #return - o valor de (b ^ p) % mod
+ * #complexidade - O(log(p))
+**/
+
+ll fastpow(ll b, ll expo, ll mod) {
+  ll ret = 1, pot = b % mod;
+  while (expo){
+    if (expo & 1) {
+      ret = (ret * pot) % mod;
+    }
+    pot = (pot * pot) % mod;
+    expo >>= 1;
+  }
   return ret;
 }
