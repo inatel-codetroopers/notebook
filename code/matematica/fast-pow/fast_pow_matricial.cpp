a9bc469f37613979a0514949a412873643efd217
lucianovr <rluciano025@gmail.com>
2019-09-11 00:02:02 -0300

    Added fast pow

--- code/matematica/fast-pow/fast_pow_matricial.cpp
+++ code/matematica/fast-pow/fast_pow_matricial.cpp
@@ -8,32 +8,32 @@
 **/
 
 #define MAX (ChangeMe) // Max size of square matrix
-struct matrix_t{
+
+struct matrix_t {
   ll m[MAX][MAX];
-  int size;
-  matrix_t multiply(const matrix_t q, ll mod){
-    matrix_t ret;
-    ret.size = size;
-    rep(i,0,size) rep(j,0,size){
+  int sz;
+  matrix_t(int _sz) {
+    memset(m, 0, sizeof(m));
+    sz = _sz;
+  }
+  matrix_t multiply(const matrix_t other, ll mod) {
+    matrix_t ret(other.sz);
+    rep(i, 0, sz) rep(j, 0, sz) {
       ret.m[i][j] = 0;
-      rep(k,0,size){
-        ret.m[i][j] = (ret.m[i][j] + (m[i][k] * q.m[k][j]) % mod) % mod;
-      }
+      rep(k, 0, sz) ret.m[i][j] =
+          (ret.m[i][j] + (m[i][k] * other.m[k][j]) % mod) % mod;
     }
     return ret;
   }
 };
 
+matrix_t fastpow(matrix_t M, ll expo, ll mod) {
+  matrix_t ret(M.sz);
+  // Identity matrix
+  rep(i, 0, ret.sz) rep(j, 0, ret.sz) ret.m[i][j] = (i == j);
 
-matrix_t fastpow(matrix_t M, ll expo, ll mod){
-  matrix_t ret;
-  ret.size = M.size;
-  rep(i,0,ret.size)
-    rep(j,0,ret.size)
-      ret.m[i][j] = (i == j); // init Identity matrix
-
-  while (expo){
-    if(expo & 1)
+  while (expo) {
+    if (expo & 1)
       ret = ret.multiply(M, mod);
     M = M.multiply(M, mod);
     expo >>= 1;
--- code/matematica/fast-pow/testes/BIGMOD.cpp
+++ code/matematica/fast-pow/testes/BIGMOD.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
 #define pb push_back
 #define mp make_pair
-#define debug(x) cout<<__LINE__<<": "<<#x<<" = "<<x<<endl;
-#define debug2(x, y) cout<<__LINE__<<": "<<#x<<" = "<<x<<\
-                                     "  "<<#y<<" = "<<y<<endl;
+#define debug(x) cout << __LINE__ << ": " << #x << " = " << x << endl;
+#define debug2(x, y)                                                   \
+  cout << __LINE__ << ": " << #x << " = " << x << "  " << #y << " = "  \
+       << y << endl;
 #define all(c) (c).begin(), (c).end()
 #define F first
 #define S second
-#define UNIQUE(c) \
-    sort(all(c)); \
-    (c).resize(unique(all(c))-c.begin());
+#define UNIQUE(c)                                                      \
+  sort(all(c));                                                        \
+  (c).resize(unique(all(c)) - c.begin());
 
 #define PI 3.1415926535897932384626433832795028841971
 
@@ -23,24 +24,22 @@ typedef vector<int> vi;
 const int INF = 0x3f3f3f3f;
 const double EPS = 1e-9;
 
-inline int cmp(double x, double y = 0, double tol = EPS){
-  return ((x <= y+tol) ? (x+tol < y) ? -1:0:1);
+inline int cmp(double x, double y = 0, double tol = EPS) {
+  return ((x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1);
 }
-
-
 
 /**
  * fastpow() realiza exponenciacao rapida de inteiros
  * #param ll b - base da exponenciacao
  * #param ll expo - expoente
- * #param ll mod - o resultado sera calculado modulo este valor
+ * #param ll mod - o resultado sera calculado modulo este
+ *valor
  * #return - o valor de (b ^ p) % mod
  * #complexidade - O(log(p))
 **/
-
 ll fastpow(ll b, ll expo, ll mod) {
   ll ret = 1, pot = b % mod;
-  while (expo){
+  while (expo) {
     if (expo & 1) {
       ret = (ret * pot) % mod;
     }
@@ -50,15 +49,13 @@ ll fastpow(ll b, ll expo, ll mod) {
   return ret;
 }
 
+int main() {
 
-int main(){
+  ll B, P, M;
 
-    ll B, P, M;
+  while (cin >> B >> P >> M) {
+    cout << fastpow(B, P, M) << endl;
+  }
 
-    while (cin >> B >> P >> M)
-    {
-        cout << fastpow(B, P, M) << endl;
-    }
-
-    return 0;
+  return 0;
 }
