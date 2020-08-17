59774f73d41c009f7e6105ca83c05bfb644417a9
Adson-S <54084051+Adson-S@users.noreply.github.com>
2020-08-17 17:53:21 -0300

    Update euler_tour.cpp

--- code/grafos/euler_tour.cpp
+++ code/grafos/euler_tour.cpp
@@ -1,24 +1,10 @@
 const int MAX = 1e5+5;
 vector<int> adj[MAX];
-vector<int> lvl[MAX];
 int range[MAX][2];
-int p[MAX][21];
-int h[MAX], cnt;
 
-void dfs(int u, int pp) {
-    p[u][0] = pp;
-    h[u] = h[pp] + 1;
+void dfs(int u, int p) {
     range[u][0] = ++cnt;
-    lvl[h[u]].pb(cnt);
-    REP(i, 1, 20)
-        p[u][i] = p[p[u][i-1]][i-1];
-    for(auto v : adj[u]) dfs(v, u);
+    for(auto v : adj[u])
+        if(v != p) dfs(v, u);
     range[u][1] = cnt;
-}
-
-int ancestor(int u, int H) {
-    PER(i, 20, 0)
-        if(H & (1<<i))
-            u = p[u][i];
-    return u;
 }
