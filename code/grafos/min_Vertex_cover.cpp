37a73b9914555b35f5bca08ab3462c0270205993
Adson-S <54084051+Adson-S@users.noreply.github.com>
2020-11-09 09:50:57 -0300

    Create min_Vertex_cover.cpp

---
+++ code/grafos/min_Vertex_cover.cpp
@@ -0,0 +1,14 @@
+// Usar depois de chamar matching
+// para achar o maximo bipartido
+void minVertexCorver() {
+  vector<int> l1, l2;
+  // Lado A
+  REP(i, 1, N)
+    if(vis[i] == 0)
+      l1.pb(i);
+  // Lado B
+  REP(i, 1, M)
+    if(vis[i+N] == 1)
+      l2.pb(i+N);
+}
+	
